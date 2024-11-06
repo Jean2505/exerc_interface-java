b31f865dccdf2888b20cf1617c3b6dcd9469eb3e
Jean2505 <jean.victor12357@gmail.com>
2024-11-06 15:08:04 -0300

    first commit with no interface implementation

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,24 @@
+package model.entities;
+
+public class Vehicle {
+
+	private String model;
+	
+	// constructors
+	public Vehicle() {
+	}
+
+	public Vehicle(String model) {
+		this.model = model;
+	}
+
+	// getters and setters
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+}
